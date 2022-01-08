8deb8ea48d44485aba7c23ad370924ed6532ab49
Justice219 <65798268+Justice219@users.noreply.github.com>
2022-01-08 16:10:25 -0600

    Update cc_sv_chat.lua

--- lua/addons/chatcommands/server/cc_sv_chat.lua
+++ lua/addons/chatcommands/server/cc_sv_chat.lua
@@ -1,3 +1,21 @@
 chatcommands = chatcommands or {}
 chatcommands.server = chatcommands.server or {}
 chatcommands.net = chatcommands.net or {}
+
+local Commands = {
+    ["!implode"] = function(ply, text, ...)
+        print(...)
+    end,
+}
+
+hook.Add("PlayerSay", "RoofChatCommands", function(ply, txt)
+
+    local cmd = string.lower(args[1])
+    table.remove(args, 1)
+
+    local command = Commands[cmd]
+
+    if command then
+        command(ply, text)
+    end
+end)
